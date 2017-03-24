313064dd05de87a88741f35979a2460d6d29d791
Jhonatan Kolen <jhonatankolen@gmail.com>
2017-03-24 20:22:21 -0300

    Seção 2 - 6: Configuração inicial do arquivo de inicialização do webpack

---
+++ react-redux/secao2-webpack/exercicios/webpack.config.js
@@ -0,0 +1,18 @@
+//realizando referencia o webpack
+const webpack = require('webpack')
+
+//Cofiguração do webpack
+module.exports = {
+    //Ponte de entrar, arquivo de entrada de configuração, definicao de dependencias, etc.
+    entry: '.ex/index.js',
+    //Configuração de saida do webpack, arquivo que será geradoJhou99346554
+    output: {
+        path: __dirname + '/public', 
+        filename: './bundle.js'
+    },
+    //Configuração do servidor
+    devServer: {
+        port: 8080,
+        contentBase: ',/public'
+    }
+}
