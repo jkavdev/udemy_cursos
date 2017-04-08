9672590d57530bf4141d2ab86c46f3bf7266cb3d
jkavdev <jhonatankolen@gmail.com>
2017-04-08 11:55:29 -0300

    Seção 2 - 8: Testando configuração

--- react-redux/secao2-webpack/exercicios/webpack.config.js
+++ react-redux/secao2-webpack/exercicios/webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack')
 //Cofiguração do webpack
 module.exports = {
     //Ponte de entrar, arquivo de entrada de configuração, definicao de dependencias, etc.
-    entry: '.ex/index.js',
+    entry: './ex/index.js',
     //Configuração de saida do webpack, arquivo que será geradoJhou99346554
     output: {
         path: __dirname + '/public', 
@@ -13,6 +13,6 @@ module.exports = {
     //Configuração do servidor
     devServer: {
         port: 8080,
-        contentBase: ',/public'
+        contentBase: './public'
     }
 }
