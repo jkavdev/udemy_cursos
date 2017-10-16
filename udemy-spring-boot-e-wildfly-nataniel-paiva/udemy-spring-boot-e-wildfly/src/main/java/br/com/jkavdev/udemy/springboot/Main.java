deb68cb11943dd27924db695a1da81c0470cd6b3
jkavdev <jhonatankolen@gmail.com>
2017-10-16 13:10:28 -0200

    Seção: 1 Spring Boot e Wildfly 11 - 1. Introdução ao Wildfly 11
    - Retirando o tomcat como servidor padrão
    - Adicionando o wildfly com servidor
    - Configurando o projeto para rodar com o wildfly

--- udemy-spring-boot-e-wildfly-nataniel-paiva/udemy-spring-boot-e-wildfly/src/main/java/br/com/jkavdev/udemy/springboot/Main.java
+++ udemy-spring-boot-e-wildfly-nataniel-paiva/udemy-spring-boot-e-wildfly/src/main/java/br/com/jkavdev/udemy/springboot/Main.java
@@ -2,14 +2,24 @@ package br.com.jkavdev.udemy.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 //Anotacao que indica para o spring boot iniciar todo o seu contexto
 @SpringBootApplication
-public class Main {
+// SpringBootServletInitializer podemos alterar o comportamento padrao
+// dos servlet, que no caso iremos alterar o servidor, utilizando o wildfly
+public class Main extends SpringBootServletInitializer {
 
-	//Inicializa o contexto
+	// Inicializa o contexto
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Main.class, args);
+	}
+
+	// Para indicar alteracao no servidor, precisamos reescrever o metodo configure
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(getClass());
 	}
 
 }
