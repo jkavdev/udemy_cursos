4676102ed4a45c4f6d7947d0037785eae9d95904
jkavdev <jhonatankolen@gmail.com>
2018-05-17 12:46:43 -0300

    # Curso MEAN - Backend - Ciclo de Pagamento API
    53. Testando a API de Ciclo de Pagamentos (Parte 2)

--- mongo-express-angular-node-primeira-app/curso-frames-web/backend/api/billingCycle/billingCycle.js
+++ mongo-express-angular-node-primeira-app/curso-frames-web/backend/api/billingCycle/billingCycle.js
@@ -2,12 +2,12 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
-    name: { type: String, require: true },
+    name: { type: String, required: true },
     value: { type: Number, min: 0, required: true }
 })
 
 const debtSchema = new mongoose.Schema({
-    name: { type: String, require: true },
+    name: { type: String, required: true },
     value: { type: Number, min: 0, required: [true, 'Informe o valor do débito'] },
     status: {
         type: String, require: false, uppercase: true,
@@ -16,7 +16,7 @@ const debtSchema = new mongoose.Schema({
 })
 
 const billingCycleSchema = new mongoose.Schema({
-    name: { type: String, require: true },
+    name: { type: String, required: true },
     month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
     credits: [creditSchema],
