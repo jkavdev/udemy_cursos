bcfdedb53debbbba66ac17af9d096c4b3cc16b05
jkavdev <jhonatankolen@gmail.com>
2018-05-19 20:47:24 -0300

    # Curso MEAN - Frontend - Cadastro de Ciclo de Pagamentos - fix
    83. Componente Campo de Formulário (Field)

--- mongo-express-angular-node-primeira-app/curso-frames-web/frontend/app/billingCycle/billingCycleController.js
+++ mongo-express-angular-node-primeira-app/curso-frames-web/frontend/app/billingCycle/billingCycleController.js
@@ -7,8 +7,12 @@
 
     function BillingCycleController($http) {
         const vm = this
+        vm.billingCycle = {}
 
         vm.create = function () {
+
+            console.log(vm.billingCycle)
+
             const url = 'http://localhost:3003/api/billingSummary'
             $http.post(url, vm.billingCycle)
                 .then(function (response) {
