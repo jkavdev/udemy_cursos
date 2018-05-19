95745e786081edef75f92422a23a370d3a0ad7cb
jkavdev <jhonatankolen@gmail.com>
2018-05-19 14:58:04 -0300

    # Curso MEAN - Frontend - Dashboard
    75. Componente ValueBox

--- mongo-express-angular-node-primeira-app/curso-frames-web/frontend/app/common/components/contentHeader.js
+++ mongo-express-angular-node-primeira-app/curso-frames-web/frontend/app/common/components/contentHeader.js
@@ -1,9 +1,11 @@
 angular.module('primeiraApp').component('contentHeader', {
-    bindings: {
-        name: '@',
-        small: '@',
-    },
-    template: `
+    bindings:
+        {
+            name: '@',
+            small: '@',
+        },
+    template:
+        `
         <section class="content-header">
             <h1>{{ $ctrl.name }} <small>{{ $ctrl.small }}</small> </h1>
         </section>
