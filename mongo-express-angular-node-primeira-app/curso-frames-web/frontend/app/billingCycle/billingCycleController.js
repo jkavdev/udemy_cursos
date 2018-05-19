64d7b884b673ba06566f37111f1867ac4b95621e
jkavdev <jhonatankolen@gmail.com>
2018-05-19 18:38:25 -0300

    # Curso MEAN - Frontend - Cadastro de Ciclo de Pagamentos
    85. Atualizando o BillingCycleController para Angular 1.6

--- mongo-express-angular-node-primeira-app/curso-frames-web/frontend/app/billingCycle/billingCycleController.js
+++ mongo-express-angular-node-primeira-app/curso-frames-web/frontend/app/billingCycle/billingCycleController.js
@@ -7,14 +7,13 @@
 
     function BillingCycleController($http) {
         const vm = this
-        const url = 'http://localhost:3003/api/billingSummary'
 
         vm.create = function () {
-            console.log('Tudo funcionando........')
-            // $http.post(url, vm.billingCycle)
-            //     .then(function (response) {
-            //         console.log('Tudo funcionando........')
-            //     })
+            const url = 'http://localhost:3003/api/billingSummary'
+            $http.post(url, vm.billingCycle)
+                .then(function (response) {
+                    console.log('Tudo funcionando........')
+                })
         }
 
     }
