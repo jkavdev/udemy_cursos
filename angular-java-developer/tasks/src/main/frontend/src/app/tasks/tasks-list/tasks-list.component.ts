f085879b17715fecb7c6c2fa19f13a4bfd2a341d
jkavdev <jhonatankolen@gmail.com>
2018-11-27 21:52:19 -0200

    Secao 2 - Building a Simple Tasks Application with Angular and Spring Boot
    16. Angular Tasks List Component
    
    Angular 4 Java Developer

--- angular-java-developer/tasks/src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ angular-java-developer/tasks/src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
+import {Task} from '../task.model';
+
 @Component({
     selector: 'app-tasks-list',
     templateUrl: './tasks-list.component.html',
@@ -7,10 +9,22 @@ import {Component, OnInit} from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
+    tasks: Task[] = []
+
     constructor() {
     }
 
     ngOnInit() {
+        this.tasks.push(new Task(1, "Task1", true, "27/11/2018"));
+        this.tasks.push(new Task(1, "Task1", false, "27/11/2018"));
+        this.tasks.push(new Task(1, "Task1", false, "27/11/2018"));
     }
 
+    onTaskChange($event, task: Task) {
+        console.log('A tarefa mudou');
+    }
+
+    getDueDateLabel(task: Task) {
+        return task.completed ? 'label-success' : 'label-primary';
+    }
 }
